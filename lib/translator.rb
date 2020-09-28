9b99b70956cbca0e86353ada8369a340e3996699
Dominique Brisco <dbrisco93@gmail.com>
2020-09-28 07:56:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,10 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   # code goes here
-  load_library(path)
+  load_library(path).each do |key, value|
+    if value[:english] == emoticon
+      return key
+    end
 end
 
 def get_english_meaning(path, emoticon)
