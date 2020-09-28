5626b6f48401b6579dac834cced01355d635b8c2
Dominique Brisco <dbrisco93@gmail.com>
2020-09-28 07:17:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,8 @@ def load_library(emoticons)
   new_hash = {}
   YAML.load_file(emoticons).each do |key, value|
     new_hash[key] = {}
-    new_hash[key][:english] = ""
-    new_hash[key][:japanese] = ""
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
     binding.pry
   end
 
