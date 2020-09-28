b72863a790607d522e3c9c8238dbcfa4b7e2eb5a
Dominique Brisco <dbrisco93@gmail.com>
2020-09-28 07:51:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,8 +23,8 @@ def get_english_meaning(path, emoticon)
   load_library(path).each do |key, value|
     if  value[:japanese] == emoticon
       return key
-    elseif 
+    else
       puts "Sorry, that emoticon  was not found"
-  end
+   end
   end
 end
